437ccd9e52bba07491b1302cd9814c2204fe7d9a
Jair Benitez <jair.benitez@gmail.com>
2019-02-09 17:25:45 -0600

    Initial Commit

---
+++ 10-ChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  10-ChatApp
+//
+//  Created by Jair Benítez on 09/02/19.
+//  Copyright © 2019 Jair Benítez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
