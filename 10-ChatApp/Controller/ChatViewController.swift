ee07125a75ccd495d20f2aa0e610b165148f320c
Jair Benitez <jair.benitez@gmail.com>
2019-02-23 19:15:24 -0600

    Login en firebase

--- 10-ChatApp/Controller/ChatViewController.swift
+++ 10-ChatApp/Controller/ChatViewController.swift
@@ -7,16 +7,40 @@
 //
 
 import UIKit
+import Firebase
 
 class ChatViewController: UIViewController {
 
+    @IBOutlet weak var messagesTableView: UITableView!
+    @IBOutlet weak var massageTextField: UITextField!
+    @IBOutlet weak var sendButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func sendPressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func logoutPressed(_ sender: UIButton) {
+        do{
+            try Auth.auth().signOut()
+        
+        } catch {
+            print("No sea a podido usar signout")
+        }
+        
+        guard (navigationController?.popToRootViewController(animated: true)) != nil else {
+            print("no hay view controlles a eliminar en el stack")
+            return
+        }
+        
+        
+        
+    }
+    
     /*
     // MARK: - Navigation
 
